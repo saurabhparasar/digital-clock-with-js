efcaafbc682ddf91f94a217eadc7801f27b31f23
saurabh parasar <saurabhparasar3010@gmail.com>
2021-06-06 19:39:20 +0530

    updated

--- clock.js
+++ clock.js
@@ -3,4 +3,17 @@ setInterval(function () {
   var hours = currentDate.getHours();
   var minutes = currentDate.getMinutes();
   var seconds = currentDate.getSeconds();
+  var period = "AM";
+  if (hours >= 12) {
+    period = "PM";
+  }
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
 });
